a9988ff824d338a4002b9d6aa30840ba9fad9f85
Anderson Vasconcelos <ciclista-rs@hotmail.com>
2021-08-04 19:51:51 -0300

    feat post

--- pet/views.py
+++ pet/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from pet.models import Group, Characteristic
 from .serializers import GroupSerializer
 from .serializers import CharacteristicSerializer
+from rest_framework import status
 
 class GroupView(APIView):
     def get(self, request):
@@ -11,10 +12,23 @@ class GroupView(APIView):
         serializer = GroupSerializer(group, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        
+        print(request.data)
+        serializer = GroupSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        group = Group.objects.get_or_create(**serializer.validated_data)[0]
+        serializer = GroupSerializer(group)
+
+        return Response(serializer.data)
+
+
 class CharacteristicView(APIView):
     def get(self, request):
-        name = Characteristic.objects.all()
-        serializer = CharacteristicSerializer(name)
+        charac = Characteristic.objects.all()
+        serializer = CharacteristicSerializer(charac)
         return Response(serializer.data)
 
 
